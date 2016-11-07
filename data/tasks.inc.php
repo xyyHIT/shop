16d48e0e760f25b1815d50ea640f98354c3293ab
Gavin <tianyu_0723@hotmail.com>
2016-11-07 16:38:40 +0800

    dev

--- data/tasks.inc.php
+++ data/tasks.inc.php
@@ -5,8 +5,8 @@ return array (
   array (
     'cycle' => 'custom',
     'interval' => 3600,
-    'due_time' => 1478168061,
-    'last_time' => 1478164461,
+    'due_time' => 1478489465,
+    'last_time' => 1478485865,
   ),
 );
 
--- temp/caches/0386/3650aba1f22b46088bdd773a10d59113.cache.php
+++ temp/caches/0386/3650aba1f22b46088bdd773a10d59113.cache.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *  @Created By ECMall PhpCacheServer
- *  @Time:2016-10-31 04:27:40
+ *  @Time:2016-11-07 09:29:25
  */
 
 if(filemtime(__FILE__) + 86400 < time())return false;
