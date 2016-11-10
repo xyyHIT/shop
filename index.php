5c20c0f3b948c2bc7aea7c5b56035222786fc02d
he <he@gmail.com>
2016-11-10 10:32:30 +0800

    去掉

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define('ROOT_PATH', dirname(__FILE__));
 include(ROOT_PATH . '/eccore/ecmall.php');
 
 // composer 自动加载
-include ROOT_PATH.'/vendor/autoload.php';
+//include ROOT_PATH.'/vendor/autoload.php';
 
 /* 定义配置信息 */
 ecm_define(ROOT_PATH . '/data/config.inc.php');
@@ -13,17 +13,11 @@ $ECMall = new ECMall();
 // 配置时区
 date_default_timezone_set('PRC');
 
-if(isset($_GET['api'])){
-	$app_con = '/wxapp';
-}else{
-	$app_con = '/app';
-}
-
 /* 启动ECMall */
 $ECMall->startup(array(
     'default_app'   =>  'default',
     'default_act'   =>  'index',
-    'app_root'      =>  ROOT_PATH . $app_con,
+    'app_root'      =>  ROOT_PATH . '/wxapp',
     'external_libs' =>  array(
         ROOT_PATH . '/includes/global.lib.php',
         ROOT_PATH . '/includes/libraries/time.lib.php',
