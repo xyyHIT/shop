26df94bf7e03c319dd08b44467eed8981ff0cf5d
he <he@gmail.com>
2016-11-25 15:10:03 +0800

    调整提交

--- wxapp/goods.app.php
+++ wxapp/goods.app.php
@@ -237,9 +237,9 @@
                 $this->set_store($data['goods']['store_id']);
             }
 			$colstore =  $this->_goods_mod->db->getOne("select user_id from ".DB_PREFIX."collect where type='store' and item_id=".$data['goods']['store_id']." and user_id=".$this->visitor->get('user_id'));
-			$result['store']['collectsign'] = empty($colstore)?'0':'1';
+			$data['store']['collectsign'] = empty($colstore)?'0':'1';
 			$colsgoods =  $this->_goods_mod->db->getOne("select user_id from ".DB_PREFIX."collect where type='goods' and item_id=".$data['goods']['goods_id']." and user_id=".$this->visitor->get('user_id'));
-			$result['goods']['collectsign'] = empty($colsgoods)?'0':'1'; 
+			$data['goods']['collectsign'] = empty($colsgoods)?'0':'1'; 
             return $data;
         }
 
