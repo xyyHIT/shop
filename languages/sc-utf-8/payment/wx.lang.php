0c52c0131f5faafd2eec9eeecc8dc2722ef87894
he <he@gmail.com>
2016-11-25 12:46:36 +0800

    微信支付插件2

---
+++ languages/sc-utf-8/payment/wx.lang.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    'wxpay'  => '微信支付',
+    'wxpay_desc'   => '艺加商城微信支付',
+    'wxpay_currency'   => '人民币',
+    'alipay_account'    => '微信账号',
+    'alipay_account_desc'   => '输入您在支付宝的账号',
+    'alipay_key'            => '交易安全校验码（key）',
+    'alipay_key_desc'       => '<a href="https://www.alipay.com/himalayas/product_info.htm?type=from_agent_contract&id=C4335319945672464113" target="_blank">立即免费签约支付宝，获取校验码和ID</a>',
+    'alipay_partner'        => '合作者身份（partner ID）',
+    'alipay_service'        => '接口类型',
+    'alipay_service_desc'   => '1.5%费率用户请选“担保交易接口”',
+    'trade_create_by_buyer' => '标准双接口',
+    'create_partner_trade_by_buyer' => '担保交易接口',
+    'create_direct_pay_by_user' => '即时到帐交易接口',
+);
+
+?>
