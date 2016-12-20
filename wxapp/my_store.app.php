faff889a34ec16461dc52197f45dc0dacd81db79
he <he@gmail.com>
2016-12-20 16:01:55 +0800

    添加用户订单统计

--- wxapp/my_store.app.php
+++ wxapp/my_store.app.php
@@ -39,6 +39,7 @@
             // 总收入
             $grossIncome = $userModel->getOne('select sum(goods_amount) from '.DB_PREFIX.'order where status = 40 and seller_id = '.$userID);
             $ret['grossIncome'] = empty($grossIncome) ? 0 : $grossIncome;
+			$ret['order_sta']=get_stats($this->visitor->get('user_id'));	//获取订单各个状态的数量
 
             return $this->ej_json_success($ret);
         }
