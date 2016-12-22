4bde53af12931caab1fd651a6e8a52c418eb454c
he <he@gmail.com>
2016-12-22 15:28:29 +0800

    添加余额支付后台部分

---
+++ includes/payments/balance/payment.info.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'code'      => 'balance',
+    'name'      => Lang::get('balpay'),
+    'desc'      => Lang::get('balpay_desc'),
+    'is_online' => '1',
+    'author'    => '艺加PHP',
+    'website'   => '',
+    'version'   => '1.0',
+    'currency'  => Lang::get('balpay_currency'),
+);
+
+?>
---
+++ languages/sc-utf-8/payment/balance.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'balpay'  => '余额支付',
+    'balpay_desc'   => '艺加商城余额支付',
+    'balpay_currency'   => '人民币',
+    'balpay_account'    => '账号',
+);
+
+?>
