6f6494c180e5671b90b5b0c32b2229be7a0547a6
Gavin <tianyu_0723@hotmail.com>
2017-01-09 16:22:16 +0800

    分类商品按照分析表score倒序排列

--- wxapp/catinfo.app.php
+++ wxapp/catinfo.app.php
@@ -37,12 +37,14 @@ class CatinfoApp extends MallbaseApp
 			foreach($stats['by_category'] as $v){
 				$sql .= "(select g.goods_id,g.default_image,g.goods_name,g.price,g.cate_id,gs.sales from ".DB_PREFIX."goods g ".
 					" LEFT JOIN ".DB_PREFIX."goods_statistics gs ON gs.goods_id = g.goods_id ". 
-					" LEFT JOIN ".DB_PREFIX."store s ON g.store_id = s.store_id  where g.if_show = 1 AND g.closed = 0 AND s.state = 1 and cate_id=".$v['cate_id']." limit 5)";
+					" LEFT JOIN ".DB_PREFIX."store s ON g.store_id = s.store_id  where g.if_show = 1 " .
+					" AND g.closed = 0 AND s.state = 1 and cate_id=".$v['cate_id']." order by gs.score desc limit 5)";
 				if($v['cate_id'] != $endarr['cate_id']){
 					$sql .= " union all ";	
 				}
 				
 			}
+Log::getLogger()->alert($sql);
 			$goods_list = $goods_mod->getAll($sql);
 		}
 
