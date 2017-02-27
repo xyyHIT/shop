0170b3184c4c6ff1104112b6fea50e31ff38523c
he <he@gmail.com>
2017-02-27 10:42:04 +0800

    整合代码buyer——order

--- wxapp/buyer_order.app.php
+++ wxapp/buyer_order.app.php
@@ -9,25 +9,22 @@
 class Buyer_orderApp extends MemberbaseApp
 {
 
-    //var $ejstatus = array('0'=>'交易取消','11'=>'待付款','20'=>'待发货','30'=>'待收货','40'=>'交易完成');
-    var $ejstatus = [ '0' => '交易关闭', '11' => '等待买家付款', '20' => '买家已付款', '30' => '卖家已发货', '40' => '交易完成' ];
-
-    /**
-     * 评价订单
-     *
-     * by Gavin 20161116
-     */
-    function ejEvaluate()
-    {
-        // 订单ID不能为空
+		//var $ejstatus = array('0'=>'交易取消','11'=>'待付款','20'=>'待发货','30'=>'待收货','40'=>'交易完成');
+		var $ejstatus = array('0'=>'交易关闭','11'=>'等待买家付款','20'=>'买家已付款','30'=>'卖家已发货','40'=>'交易完成');
+        /**
+         * 评价订单
+         *
+         * by Gavin 20161116
+         */
+        function ejEvaluate() {
+			// 订单ID不能为空
         $order_id = isset($_REQUEST['order_id']) ? intval($_REQUEST['order_id']) : 0;
         $descStars = isset($_REQUEST['desc_stars']) ? intval($_REQUEST['desc_stars']) : 5; // 描述评级
         $logiStars = isset($_REQUEST['logi_stars']) ? intval($_REQUEST['logi_stars']) : 5; // 物流评级
         $servStars = isset($_REQUEST['serv_stars']) ? intval($_REQUEST['serv_stars']) : 5; // 服务评级
-
-        if ( !$order_id ) {
-            return $this->ej_json_failed(-1, Lang::get('no_such_order'));
-        }
+            if ( !$order_id ) {
+                return $this->ej_json_failed(-1, Lang::get('no_such_order'));
+            }
 
         // 现在改成传json 字符串
         $evaluationArr = json_decode(stripslashes($_REQUEST['evaluations']), true);
@@ -408,7 +405,7 @@ class Buyer_orderApp extends MemberbaseApp
         $data['order_id'] = $order_id;
         include ROOT_PATH . '/includes/aes.base.php';
         $serialjson = Security::encrypt(json_encode($data), 'yijiawang.com#@!');
-        $this->_confirmcurl([ 'data' => $serialjson ]);
+    	$this->_confirmcurl(array('data'=>$serialjson));
         //推送卖家确认收货消息
         $data = [
             'first'    => '报告主人，订单' . $sreamarr['order_sn'] . '买家已确认收货，小艺把钱已装入您的钱包,请注意查收哦！',
@@ -552,230 +549,6 @@ class Buyer_orderApp extends MemberbaseApp
         }
     }
 
-    /**
-     *    获取订单列表
-     *
-     * @author    Garbin
-     * @return    void
-     */
-    function _get_orders()
-    {
-        $page = $this->_get_page(10);
-        $model_order =& m('order');
-        !$_GET['type'] && $_GET['type'] = 'all_orders';
-        $con = [
-            [      //按订单状态搜索
-                'field'   => 'status',
-                'name'    => 'type',
-                'handler' => 'order_status_translator',
-            ],
-            [      //按店铺名称搜索
-                'field' => 'seller_name',
-                'equal' => 'LIKE',
-            ],
-            [      //按下单时间搜索,起始时间
-                'field'   => 'add_time',
-                'name'    => 'add_time_from',
-                'equal'   => '>=',
-                'handler' => 'gmstr2time',
-            ],
-            [      //按下单时间搜索,结束时间
-                'field'   => 'add_time',
-                'name'    => 'add_time_to',
-                'equal'   => '<=',
-                'handler' => 'gmstr2time_end',
-            ],
-            [      //按订单号
-                'field' => 'order_sn',
-            ],
-        ];
-        $conditions = $this->_get_query_conditions($con);
-        /* 查找订单 */
-        $orders = $model_order->findAll([
-            'conditions' => "buyer_id=" . $this->visitor->get('user_id') . "{$conditions}",
-            'fields'     => 'this.*',
-            'count'      => true,
-            'limit'      => $page['limit'],
-            'order'      => 'add_time DESC',
-            'include'    => [
-                'has_ordergoods',       //取出商品
-            ],
-        ]);
-        //'0'=>'交易取消','11'=>'等待买家付款','20'=>'买家已付款','30'=>'卖家已发货','40'=>'交易完成'
-        $result = [];
-        if ( $orders ) {
-            foreach ( $orders as $key1 => $order ) {
-                $temp['order_id'] = $order['order_id'];
-                $temp['seller_id'] = $order['seller_id'];
-                $temp['invoice_no'] = empty($order['invoice_no']) ? '' : trim($order['invoice_no']);
-                $temp['seller_name'] = $order['seller_name'];
-                $temp['buyer_id'] = $order['buyer_id'];
-                $temp['status'] = $order['status'];
-                $temp['statusname'] = $this->ejstatus[ $order['status'] ];
-                switch ( $order['status'] ) {
-                    case 11:
-                        $temp['button'] = "<div class='dOperate'><a class='quxiaodingdan'>取消订单</a><a class='qufukuan'>去付款</a></div>";
-                        break;
-                    case 20:
-                        $temp['button'] = "<div class='dOperate'><a class='tixingfahuo'>提醒发货</a></div>";
-                        break;
-                    case 30:
-                        $temp['button'] = "<div class='dOperate'><a class='yanchangshouhuo'>延长收货</a><a class='chakanwuliu'>查看物流</a><a class='querenshouhuo'>确认收货</a></div>";
-                        break;
-                    case 40:
-                        $temp['button'] = "<div class='dOperate'><a class='chakanwuliu'>查看物流</a></div>";
-                        break;
-                    default:
-                        $temp['button'] = '';
-                }
-                $temp['order_amount'] = $order['order_amount'];
-                $tmpgoods = [];
-                foreach ( $order['order_goods'] as $v ) {
-                    $tmp['rec_id'] = $v['rec_id'];
-                    $tmp['order_id'] = $v['order_id'];
-                    $tmp['goods_id'] = $v['goods_id'];
-                    $tmp['goods_name'] = $v['goods_name'];
-                    $tmp['quantity'] = $v['quantity'];
-                    $tmp['price'] = $v['price'];
-                    $tmp['goods_image'] = $v['goods_image'];
-                    array_push($tmpgoods, $tmp);
-                }
-                $temp['order_goods'] = $tmpgoods;
-                array_push($result, $temp);
-            }
-        }
-        $page['item_count'] = $model_order->getCount();
-        $res['orderlist'] = $result;
-        $res['page'] = $page;
-
-        return $res;
-    }
-
-    function _get_member_submenu()
-    {
-        $menus = [
-            [
-                'name' => 'order_list',
-                'url'  => 'index.php?app=buyer_order',
-            ],
-        ];
-
-        return $menus;
-    }
-
-    /**
-     *    响应支付
-     *
-     * @author    Garbin
-     *
-     * @param     int   $order_id
-     * @param     array $notify_result
-     *
-     * @return    bool
-     */
-    function respond_notify( $order_id, $notify_result )
-    {
-        $model_order =& m('order');
-        $where = "order_id = {$order_id}";
-        $data = [ 'status' => $notify_result['target'] ];
-        switch ( $notify_result['target'] ) {
-            case ORDER_ACCEPTED:
-                $where .= ' AND status=' . ORDER_PENDING;   //只有待付款的订单才会被修改为已付款
-                $data['pay_time'] = gmtime();
-                break;
-            case ORDER_SHIPPED:
-                $where .= ' AND status=' . ORDER_ACCEPTED;  //只有等待发货的订单才会被修改为已发货
-                $data['ship_time'] = gmtime();
-                break;
-            case ORDER_FINISHED:
-                $where .= ' AND status=' . ORDER_SHIPPED;   //只有已发货的订单才会被自动修改为交易完成
-                $data['finished_time'] = gmtime();
-                break;
-            case ORDER_CANCLED:                             //任何情况下都可以关闭
-                /* 加回商品库存 */
-                $model_order->change_stock('+', $order_id);
-                break;
-        }
-
-        return $model_order->edit($where, $data);
-    }
-
-    /**
-     *    延长订单收货接口
-     *
-     * @author    newrain
-     * @return    void
-     */
-    function delayship()
-    {
-        $order_id = isset($_GET['order_id']) ? intval($_GET['order_id']) : 0;
-        if ( !$order_id ) {
-            return $this->ej_json_failed(2001);
-        }
-        $delayval = 1;
-        $model_order =& m('order');
-        /* 只有已发货的订单可以确认 */
-        $order_info = $model_order->get("order_id={$order_id} AND buyer_id=" . $this->visitor->get('user_id') . " AND status=" . ORDER_SHIPPED);
-        if ( $order_info['if_fronzen'] >= 1 ) {
-            return $this->ej_json_failed(1023);
-        }
-        if ( empty($order_info) ) {
-            return $this->ej_json_failed(3001);
-        } else {
-            //当用户已经点击过延长，再次点击为取消延长收货
-            if ( $order_info['add_shiptime'] ) {
-                return $this->ej_json_failed(1008);
-            }
-        }
-        $model_order->edit($order_id, [ 'add_shiptime' => $delayval ]);
-        if ( $model_order->has_error() ) {
-            return $this->ej_json_failed(3001);
-        }
-        /* 记录订单操作日志 */
-        $order_log =& m('orderlog');
-        $order_log->add([
-            'order_id'       => $order_id,
-            'operator'       => addslashes($this->visitor->get('user_name')),
-            'order_status'   => order_status($order_info['status']),
-            'changed_status' => order_status($order_info['status']),
-            'remark'         => '延长收货',
-            'log_time'       => gmtime(),
-        ]);
-
-        /*TODO 发送给卖家买家微信推送，交易完成 */
-
-        return $this->ej_json_success();
-    }
-
-    //提醒卖家发货
-    function remindship()
-    {
-        $order_id = isset($_GET['order_id']) ? intval($_GET['order_id']) : 0;
-        if ( !$order_id ) {
-            return $this->ej_json_failed(2001);
-        }
-        //获取订单信息
-        $model_order =& m('order');
-        $order_info = $model_order->get("order_id={$order_id} AND buyer_id=" . $this->visitor->get('user_id'));
-        if ( $order_info['if_fronzen'] >= 1 ) {
-            return $this->ej_json_failed(1023);
-        }
-        if ( empty($order_info) ) {
-            return $this->ej_json_failed(3001);
-        }
-        //判断是否属于待付款的状态
-        if ( $order_info['status'] != ORDER_ACCEPTED ) {
-            return $this->ej_json_failed(3001);
-        }
-        $remindtime = time();
-        if ( $order_info['remindtime'] == 0 ) {
-            $model_order->edit($order_id, [ 'remindtime' => $remindtime ]);
-        } else {
-            $diffremindtime = $remindtime - $order_info['remindtime'];
-            if ( $diffremindtime >= 86400 ) {
-                $model_order->edit($order_id, [ 'remindtime' => $remindtime ]);
-            } else {
-                return $this->ej_json_failed(1007);
         /**
          *    获取订单列表
          *
@@ -987,39 +760,59 @@ class Buyer_orderApp extends MemberbaseApp
             if ( !$order_id ) {
 				return $this->ej_json_failed(2001);
             }
-        }
-        //获取商家openid
-        $model_member =& m('member');
-        $member_info = $model_member->get("user_id=" . $order_info['seller_id'] . " AND user_id !=" . $this->visitor->get('user_id'));
-        /*TODO 发送给卖家买家微信推送，交易完成 */
-        $templateid = REMIND_SELLER;//消息模板id
-        $topenid = $member_info['openid'];
-        $data = [
-            'first'    => '买家已经催单了，老板赶紧去发货吧!',
-            'keyword1' => $order_info['order_sn'],
-            'keyword2' => $order_info['order_amount'],
-            'keyword3' => $order_info['buyer_name'],
-            'keyword4' => '已支付',
-            'remark'   => '客户已经付款，老板快去发货吧',
-        ];
-        $result = Wechat::sendNotice($topenid, $templateid, $data, SITE_URL . "/shop/html/order/orderDetail.html?orderId=" . $order_id . "&type=1");
-
-        return $this->ej_json_success();
+			//获取订单信息
+			$model_order =& m('order');
+            $order_info = $model_order->get("order_id={$order_id} AND buyer_id=" . $this->visitor->get('user_id'));
+			if($order_info['if_fronzen'] >= 1){
+				return $this->ej_json_failed(1023);
+			}
+			if(empty($order_info)){
+				return $this->ej_json_failed(3001);
+			}
+			//判断是否属于待付款的状态
+			if($order_info['status'] != ORDER_ACCEPTED){
+				return $this->ej_json_failed(3001);
+			}
+			$remindtime = time();
+			if($order_info['remindtime'] == 0){
+				$model_order->edit($order_id, [ 'remindtime' => $remindtime ]);
+			}else{
+				$diffremindtime = $remindtime - $order_info['remindtime'];
+				if($diffremindtime >= 86400){
+					$model_order->edit($order_id, [ 'remindtime' => $remindtime ]);
+				}else{
+					return $this->ej_json_failed(1007);
+				}
+			}
+			//获取商家openid
+			$model_member =& m('member');
+            $member_info = $model_member->get("user_id=".$order_info['seller_id']." AND user_id !=" . $this->visitor->get('user_id'));
+			/*TODO 发送给卖家买家微信推送，交易完成 */
+			$templateid = REMIND_SELLER;//消息模板id
+			$topenid = $member_info['openid'];
+			$data = [
+				'first'=>'买家已经催单了，老板赶紧去发货吧!',
+				'keyword1'=>$order_info['order_sn'],
+				'keyword2'=>$order_info['order_amount'],
+				'keyword3'=>$order_info['buyer_name'],
+				'keyword4'=>'已支付',
+				'remark'=>'客户已经付款，老板快去发货吧',
+			];
+			$result = Wechat::sendNotice($topenid,$templateid,$data,SITE_URL."/shop/html/order/orderDetail.html?orderId=".$order_id."&type=1");
+			return $this->ej_json_success();
+		}
+		
+		function _confirmcurl($curlPost){
+			//初始化
+			$ch = curl_init("http://tst.yijiapai.com/yjpai/common/tools/addAccountCheck");
+			//设置
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_POST, 1);    //设施post方式提交数据
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);    //设置POST的数据
+			//执行会话并获取内容
+			$output = curl_exec($ch);
+			curl_close($ch);
+			return json_decode($output,true);
+		}
+		
     }
-
-    function _confirmcurl( $curlPost )
-    {
-        //初始化
-        $ch = curl_init("http://tst.yijiapai.com/yjpai/common/tools/addAccountCheck");
-        //设置
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);    //设施post方式提交数据
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);    //设置POST的数据
-        //执行会话并获取内容
-        $output = curl_exec($ch);
-        curl_close($ch);
-
-        return json_decode($output, true);
-    }
-
-}
